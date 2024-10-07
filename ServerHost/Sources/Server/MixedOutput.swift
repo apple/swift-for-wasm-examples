51bef991689a6744c3e53620168212c4c6cd69f7
Max Desiatov <m_desiatov@apple.com>
2024-10-07 22:35:19 +0100

    Clean up formatting

--- ServerHost/Sources/Server/MixedOutput.swift
+++ ServerHost/Sources/Server/MixedOutput.swift
@@ -27,40 +27,38 @@ struct MixedOutput: ResponseGenerator {
         var memoryIndex = 0
 
         let runtime = Runtime(
-            hostModules: ["audio": .init(
-                functions: [
-                    "encode": HostFunction(type: .init(
-                        parameters: [.i32, .i32, .i32]
-                    )) { caller, args in
-                        let start = args[1].i32
-                        let byteCount = args[2].i32
+            hostModules: ["audio": .init(functions: [
+                "encode": HostFunction(type: .init(
+                    parameters: [.i32, .i32, .i32]
+                )) { caller, args in
+                    let start = args[1].i32
+                    let byteCount = args[2].i32
 
-                        // Read audio buffer from Wasm linear memory.
-                        caller.runtime.store.memory(
-                            at: memoryIndex
-                        ).data[Int(start)..<Int(start + byteCount)].withUnsafeBytes {
-                            // Rebind memory bytes to `Float32`.
-                            $0.withMemoryRebound(to: Float32.self) {
-                                // Enumerate each floating point sample
-                                for (i, sample) in $0.enumerated() {
-                                    // Extend `samples` buffer if needed.
-                                    if samples.count < $0.count {
-                                        // 0.0 (no signal) is the default sample.
-                                        samples.append(0.0)
-                                    }
-
-                                    // Mix current sample with an existing value.
-                                    samples[i] += sample
+                    // Read audio buffer from Wasm linear memory.
+                    caller.runtime.store.memory(
+                        at: memoryIndex
+                    ).data[Int(start)..<Int(start + byteCount)].withUnsafeBytes {
+                        // Rebind memory bytes to `Float`.
+                        $0.withMemoryRebound(to: Float.self) {
+                            // Enumerate each floating point sample
+                            for (i, sample) in $0.enumerated() {
+                                // Extend `samples` buffer if needed.
+                                if samples.count < $0.count {
+                                    // 0.0 (no signal) is the default sample.
+                                    samples.append(0.0)
                                 }
 
-                                assert(samples.count == $0.count)
+                                // Mix current sample with an existing value.
+                                samples[i] += sample
                             }
-                        }
 
-                        return []
+                            assert(samples.count == $0.count)
+                        }
                     }
-                ]
-            )]
+
+                    return []
+                }
+            ])]
         )
 
         // Instantiate each Wasm module

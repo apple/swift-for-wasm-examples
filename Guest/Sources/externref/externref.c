c8aaa4268e9032fedec4172db16bfd40946d16ec
Max Desiatov <m_desiatov@apple.com>
2024-12-16 18:04:32 +0000

    Fix up example code

--- Guest/Sources/externref/externref.c
+++ Guest/Sources/externref/externref.c
@@ -1,33 +1,36 @@
+#include "externref.h"
+
+struct ExternRefIndex {
+  int index;
+};
 
 static __externref_t table[0];
 
-// __externref_t test_builtin_wasm_table_get(int index) {
-//   return __builtin_wasm_table_get(table, index);
-// }
-//
-// void test_builtin_wasm_table_set(int index, __externref_t ref) {
-//   return __builtin_wasm_table_set(table, index, ref);
+__externref_t test_builtin_wasm_table_get(ExternRefIndex index) {
+  return __builtin_wasm_table_get(table, index.index);
+}
 
-int test_builtin_wasm_table_size() {
+void test_builtin_wasm_table_set(ExternRefIndex index, __externref_t ref) {
+  return __builtin_wasm_table_set(table, index.index, ref);
+}
+
+int test_builtin_wasm_table_size(void) {
   return __builtin_wasm_table_size(table);
 }
 
-// int test_builtin_wasm_table_grow(__externref_t ref, int nelem) {
-//   return __builtin_wasm_table_grow(table, ref, nelem);
-// }
-//
-// void test_builtin_wasm_table_fill(int index, __externref_t ref, int nelem) {
-//   __builtin_wasm_table_fill(table, index, ref, nelem);
-// }
-//
-// static __externref_t other_table[0];
-//
-// void test_table_copy(int dst_idx, int src_idx, int nelem) {
-//   __builtin_wasm_table_copy(table, other_table, dst_idx, src_idx, nelem);
-// }
-//
-// __attribute__((export_name("entrypoint")))
-// void entrypoint(__externref_t ref) {
-//   __builtin_wasm_table_set(table, 0, ref);
-// }
-//
+int test_builtin_wasm_table_grow(__externref_t ref, int nelem) {
+  return __builtin_wasm_table_grow(table, ref, nelem);
+}
+
+void test_builtin_wasm_table_fill(int index, __externref_t ref, int nelem) {
+  __builtin_wasm_table_fill(table, index, ref, nelem);
+}
+
+__attribute__((export_name("entrypoint")))
+void entrypoint(__externref_t ref) {
+  __builtin_wasm_table_set(table, 0, ref);
+}
+
+int foo(void) {
+  return 42;
+}
